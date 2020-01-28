f8368c6b6f733664ff1552a1d8133fcf9659c883
Hemstreet <jhemstreet@dxc.com>
2020-01-28 10:19:26 -0600

    Add project files.

---
+++ HeelloWorld/heelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main(void)
+{
+	std::cout << "Heeeeeeelllllllooooooooo!!!!!!!!" << std::endl;
+
+	std::cout << "Press ENTER to continue..." << std::endl;
+	std::cin.get();
+	
+	return 0;
+}
